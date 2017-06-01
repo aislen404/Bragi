423406166b04962f2af8d558f8123e299d8edacb
Jose Samuel Berigüete Valero <aislen404@gmail.com>
2017-06-01 20:16:03 +0200

    Art : Icons and Screens.

--- www/js/bragi.js
+++ www/js/bragi.js
@@ -15,8 +15,8 @@ var bragi = {
 		resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
 		formats : 'QR_CODE,PDF_417', // default: all but PDF_417 and RSS_EXPANDED
 		orientation : 'landscape', // Android only (portrait|landscape), default unset so it rotates with the device
-		disableAnimations : false, // iOS
-		disableSuccessBeep: false // iOS
+		disableAnimations : true, // iOS
+		disableSuccessBeep: true // iOS
 	},
 	activeBarScan: function (callback){
 	   cordova.plugins.barcodeScanner.scan(
